7134e6d648306f05aec18b6357f08ea8f4b20752
Whofelisberto <felisberto563@gmail.com>
2025-04-02 11:07:49 -0300

    first commit

---
+++ transformação-de-dados.py
@@ -0,0 +1,31 @@
+import tabula
+import zipfile
+import pandas as pd
+import os
+
+pdf_path = "anexo_I.pdf"
+
+## Função para trocar as abreviações pelas descrições 
+def trocar(df):
+    df.replace({"OD": "Odontologia", "AMB": "Ambulatório"}, inplace=True)
+    return df
+
+## usei a biblioteca tabula para extrair todas as tabelas do pdf
+tabelas = tabula.read_pdf(pdf_path, pages="all", multiple_tables=True)
+
+## concatenei as tabelas extraídas em um único dataframe
+todas_tabelas = pd.concat(tabelas, ignore_index=True)
+
+## substitui as abreviações "OD" e "AMB"
+todas_tabelas = trocar(todas_tabelas)
+
+## salvar os dados em um único arquivo CSV
+csv_path = 'tabelas.csv'
+todas_tabelas.to_csv(csv_path, index=False, encoding='utf-8')
+
+## compacta o arquivo csv para zip
+zip_path = "Teste_seu_nome.zip"
+with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+    zipf.write(csv_path, os.path.basename(csv_path))
+
+print(f"arquivo compactado em {zip_path}.")
